9b608e595f7c636130a7025334e157d202eef5f0
PATEL ADARSH <p12adarsh567@gmail.com>
2026-07-13 13:19:59 +0530

    Time: 95 ms (80.7%), Space: 39.9 MB (74.33%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun reverse(x: Int): Int {
+           var num = x
+           var rev = 0
+
+        while (num != 0) {
+            val rem = num % 10
+             if (rev > Int.MAX_VALUE / 10 || (rev == Int.MAX_VALUE / 10 && rem > 7))
+                return 0
+            if (rev < Int.MIN_VALUE / 10 || (rev == Int.MIN_VALUE / 10 && rem < -8))
+                return 0
+            rev = rev * 10 + rem
+            num /= 10
+        }
+
+        return rev
+    }
+}
