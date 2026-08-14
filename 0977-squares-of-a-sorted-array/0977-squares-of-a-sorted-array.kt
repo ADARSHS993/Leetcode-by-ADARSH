94466ed6411beea7dcc32afd85bb5c85636eb7cb
PATEL ADARSH <p12adarsh567@gmail.com>
2026-08-14 09:37:11 +0530

    Time: 1 ms (100%), Space: 50.5 MB (89.97%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.kt
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.kt
@@ -1,9 +1,22 @@
 class Solution {
     fun sortedSquares(nums: IntArray): IntArray {
-        for(i in nums.indices){
-            nums[i] = nums[i] * nums[i]
+        val ans = IntArray(nums.size)
+
+        var left = 0
+        var right = nums.size -1
+        var index = nums.size - 1
+        while(left <= right){
+            val leftSquare =  nums[left] * nums[left]
+            val rightSquare = nums[right] * nums[right]
+            if(leftSquare > rightSquare){
+                ans[index] = leftSquare
+                left++
+            }else{
+                ans[index] = rightSquare
+                right--
+            }
+            index--
         }
-        var ans = nums.sortedArray()
         return ans
     }
 }
